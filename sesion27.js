79e08451bcb400d2264f124fba2f3742802c1c60
taipicuri009 <priscilataipicuri009@gmail.com>
2021-11-28 13:45:17 -0500

    subiendo archivo

---
+++ sesion27.js
@@ -0,0 +1,26 @@
+/*function AreaCircunferencia(r){
+    const pi = 3.14;
+    area = pi * (r * r);
+    return area;
+}
+
+let pepito = prompt('Ingresa el valor del radio: ');
+resultado = AreaCircunferencia(pepito)*/
+
+function Areacuadrado(l){
+    multiplicaccion= l * l
+    return area;
+}
+ let lado = prompt('Ingresa el valor del cuadrado: ');
+resultado = Areacuadrado(lado)
+console.log(resultado);
+
+function Volumencilindro(v,h){
+    const pi = 3.14;
+    v = pi * (r * r) *h;
+    return v
+}
+
+let priscila = prompt('Ingresa el valor de la altura: ');
+resultado = Volumencilindro(priscila)
+console.log(resultado)
